0ba46a14a0a4ec5fda65c02cc61a2a88f1677ea8
Firuyee <120325749+Firuyee@users.noreply.github.com>
2023-03-22 17:10:07 +0200

    Fixed assigning min int to int.

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,26 +6,25 @@
 /*   By: tfinni <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/10 13:14:48 by tfinni            #+#    #+#             */
-/*   Updated: 2023/01/10 15:06:58 by tfinni           ###   ########.fr       */
+/*   Updated: 2023/03/20 14:59:51 by tfinni           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-
 static int	ft_intlen(int n)
 {
 	int	intlen;
-	
-	if (n == 0)
-		return (1);
+
+	if (n < -2147483647)
+		return (11);
 	intlen = 0;
 	if (n < 0)
 	{
 		intlen++;
-		n = -n;
+		n = n * -1;
 	}
-	while (n)
+	while (n > 0)
 	{
 		intlen++;
 		n = n / 10;
@@ -33,10 +32,23 @@ static int	ft_intlen(int n)
 	return (intlen);
 }
 
+static char	*ft_tochar(int len, char *s, int n)
+{
+	s[len] = '\0';
+	len--;
+	while (n > 0)
+	{
+		s[len] = n % 10 + '0';
+		n = n / 10;
+		len--;
+	}
+	return (s);
+}
+
 static char	*ft_minint(char *s)
 {
 	char	*minint;
-	int	i;
+	int		i;
 
 	minint = "-2147483648";
 	i = 0;
@@ -44,35 +56,36 @@ static char	*ft_minint(char *s)
 	{
 		s[i] = minint[i];
 		i++;
-	}		
+	}
+	s[i] = '\0';
 	return (s);
 }
 
 char	*ft_itoa(int n)
 {
-	char		*s;
+	char	*s;
 	int		len;
 
+	if (n == 0)
+	{
+		s = (char *)malloc(sizeof(char) * 2);
+		if (!s)
+			return (NULL);
+		s[0] = '0';
+		s[1] = '\0';
+		return (s);
+	}
 	len = ft_intlen(n);
-	s = (char *)malloc(sizeof(char) * (len + 1));
+	s = (char *)malloc(sizeof(char) * len + 1);
 	if (!s)
 		return (NULL);
-	s[len--] = '\0';
 	if (n < -2147483647)
 		return (ft_minint(s));
 	if (n < 0)
 	{
 		s[0] = '-';
-		n = -n;
+		n = n * -1;
 	}
-	if (n == 0)
-		s[0] = '0';
-	while (n > 0)
-	{
-		s[len] = ('0' + (n % 10));
-		n = n / 10;
-		len--;
-	}
+	ft_tochar(len, s, n);
 	return (s);
 }
-
